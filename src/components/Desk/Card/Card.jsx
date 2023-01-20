808f0b8f2d6d119f05a60c5ea933435ae1b2d999
yaukhutadzianis@gmail.com <yaukhutadzianis@gmail.com>
2023-01-20 14:15:26 +0300

    upd

--- src/components/Desk/Card/Card.jsx
+++ src/components/Desk/Card/Card.jsx
@@ -1,12 +1,15 @@
-import React, { useContext, useState } from 'react'
-import { doc, setDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import db from '../../../db';
+import { doc, setDoc, updateDoc } from 'firebase/firestore/lite';
 import styles from './Card.module.scss';
 
 function Card(props) {
+  const [id, setId] = useState(props.data.id)
   const [title, setTitle] = useState(props.data.title)
   const [clickListener, setclickListener] = useState(false)
+  const [bodyClicked, setBodyClicked] = useState(Date.now())
 
-  const onChangeCardTitle = (e) => {
+  const onChangeCardTitle = (e, id) => {
     setTitle(e.target.value)
 
     if (clickListener === false) {
@@ -14,8 +17,8 @@ function Card(props) {
 
       const body = document.getElementsByTagName('body')[0];
 
-      const listener = function (event) {
-        // updateTitle(id, title)
+      const listener = (event) => {
+        setBodyClicked(Date.now())
         body.removeEventListener("click", listener, false);
       };
 
@@ -24,25 +27,33 @@ function Card(props) {
 
   }
 
-  // const updateTitle = (id, title) => {
-  //   setDoc(
-  //     doc(db, "cards", id), {title: title}
-  //   );
-  // }
+  useEffect(() => {
+    if (title != props.data.title) {
+      console.log('if effected')
+      updateTitle();
+      setclickListener(false);
+      setBodyClicked(false)
+    }
+  }, [bodyClicked])
 
-  // useEffect(() => {
-  //   document.body.addEventListener('click', documentClickHandler);
-  //   console.log('1')
-  // }, [])
-
-  // const documentClickHandler = () => {
-  //   alert('clicked')
-  // }
+  const updateTitle = () => {
+    const docRef = doc(db, "cards", id);
+    const data = {
+      title: title
+    }
+    updateDoc(docRef, data)
+    .then(docRef => {
+        console.log("A New Document Field has been added to an existing document");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+  }
 
   return (
     <div className={`${styles.wrapper} d-flex flex-column bg-light me-3 mb-3 p-3`}>
       <div className="mb-3">
-        <input onChange={(e) => onChangeCardTitle(e)} type="email" className="form-control" placeholder="Card name" value={title} />
+        <input onChange={(e) => onChangeCardTitle(e, props.data.id)} type="email" className="form-control" placeholder="Card name" value={title} />
       </div>
     </div>
   )
